38a1d384ff3b00ce8d48641064ed7351525a48b3
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2023-05-12 16:59:43 +0000

    Ajout passwords

--- database/dataset.sql
+++ database/dataset.sql
@@ -10,22 +10,19 @@ values
     (
         'admin',
         'admin@example.com',
-        -- 'admin',
-        '$argon2id$v=19$m=65536,t=3,p=4$o0HZrFRUX9zHY69ZP3JC0w$jXOEPi0vJasnwZ8rOf5IzAhi3WoqKCXCsRvGIqqMJs0',
+        'admin',
         'admin'
     ),
     (
         'user1',
         'user1@example.com',
-        -- 'iloveu',
-        '$argon2id$v=19$m=65536,t=3,p=4$/0b6pHzhamjP9Lwsqk2q7w$bKWjza+nuPwNKFB/F1tMl5hVZfcmvgQitleBzXUDO5w',
+        'iloveu',
         'normal'
     ),
     (
         'user2',
         'user2@example.com',
-        -- 'azerty',
-        '$argon2id$v=19$m=65536,t=3,p=4$JAG3oK1FiRL05xAFA6w7mA$6XXAS8CbtayAm5wFQso8IB5svKbgOSgOu9agvkx1hjs',
+        'azerty',
         default
     ),
     (
